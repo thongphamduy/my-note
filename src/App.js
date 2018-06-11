24f8b08de215f898fa140f7ea0a44447b57670a9
thongphamduy <duythong2707@gmail.com>
2018-06-11 22:05:41 +0700

    done save to local (#7)

--- src/App.js
+++ src/App.js
@@ -2,27 +2,9 @@ import React from 'react';
 import './App.css';
 import Container from './containers/container';
 
-const data = [
-  {
-    title: 'note 1',
-    content: 'content heie',
-    id: 1,
-  },
-  {
-    title: 'note 3',
-    content: 'content heie',
-    id: 2,
-  },
-  {
-    title: 'note 4',
-    content: 'content heie',
-    id: 3,
-  },
-];
-
 const App = () => (
   <div className="container">
-    <Container data={data} />
+    <Container />
   </div>
 );
 
