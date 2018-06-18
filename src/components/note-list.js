6b5fa6ba28bafcc380a4bdad15b15979845027b7
thongphamduy <duythong2707@gmail.com>
2018-06-18 22:39:46 +0700

    added more info to note list (#10)

--- src/components/note-list.js
+++ src/components/note-list.js
@@ -6,7 +6,16 @@ const NoteList = ({
   list, onClick, activeNote,
 }) => (
   <ul className="list-group scrollable">
-    {list.map(note => <li className={`list-group-item ${activeNote === note.id ? 'selected' : null}`} key={note.id} onClick={() => onClick(note.id)}>{note.title}</li>)}
+    {list.map(note => <li
+      className={`list-group-item
+      ${activeNote === note.id ? 'selected' : null}`}
+      key={note.id}
+      onClick={() => onClick(note.id)}>
+        <h5 className="m-0 ellipsis">{note.title || 'No title'}</h5>
+        <p className="m-0 ellipsis">{note.content || 'No content'}</p>
+        <div className="text-right">{note.tags.map(tag =>
+        <span key={tag.value} className="border border-secondary rounded mr-1 mt-0 p-1 small">{tag.value}</span>)}</div>
+      </li>)}
   </ul>
 );
 
